f8bcaa743b54cc775508c2bf84f85704b369c972
Taylor J <72883059+TaylorDJones11@users.noreply.github.com>
2024-08-21 17:23:27 -0500

    adding contact to aboutus

---
+++ src/components/Contact.jsx
@@ -0,0 +1,4 @@
+function Contact() {
+  return <div className='bg-orange'>Contact</div>;
+}
+export default Contact;
--- src/pages/Home/AboutUs.jsx
+++ src/pages/Home/AboutUs.jsx
@@ -15,6 +15,7 @@ function AboutUs() {
         </div>
 
         <ProfileGrid />
+
         <Footer />
       </div>
     </>
