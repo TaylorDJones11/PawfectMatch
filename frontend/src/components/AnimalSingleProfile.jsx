76f0c4a21eca7fe2d0bf40e050d380415dc3c73b
Taylor J <72883059+TaylorDJones11@users.noreply.github.com>
2024-08-22 11:31:19 -0500

    moving items to frontend folder

---
+++ frontend/src/components/AnimalSingleProfile.jsx
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import Footer from './Footer';
+
+function AnimalProfile({
+  title,
+  imageSrc,
+  description,
+  breed,
+  age,
+  gender,
+  weight,
+}) {
+  return (
+    <div className='flex justify-center mt-12'>
+      <div className='bg-white shadow-lg rounded-lg overflow-hidden max-w-md w-full '>
+        <img src={imageSrc} alt={title} className='w-full h-52 object-cover' />
+        <div className='p-4'>
+          <Link to='/adopt'>
+            <FontAwesomeIcon
+              icon={faArrowLeft}
+              className='text-gray-700 mb-6 hover:text-gray-900 transition duration-300'
+            />
+          </Link>
+          <h2 className='text-2xl font-bold mb-2'>{title}</h2>
+          <div className='flex gap-4'>
+            <p className='text-gray-700 mb-4'>
+              <b>Breed:</b> {breed}
+            </p>
+            <p className='text-gray-700 mb-4'>
+              <b>Age:</b> {age}
+            </p>
+            <p className='text-gray-700 mb-4'>
+              <b>Gender:</b> {gender}
+            </p>
+            <p className='text-gray-700 mb-4'>
+              <b>Weight:</b> {weight}
+            </p>
+          </div>
+          <p className='text-gray-700 mb-4'>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default AnimalProfile;
